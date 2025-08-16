770d7764cebf7e3a265f1460af87c8be718b60b0
Leonardo Silva <106756853+leosilvajr@users.noreply.github.com>
2025-08-16 20:32:11 -0300

    Substituindo restante dos arquivos CSS por Styled Components

--- src/components/iconesHeader/index.js
+++ src/components/iconesHeader/index.js
@@ -1,19 +1,28 @@
 import perfil from "./../../imagens/perfil.svg";
 import sacola from "./../../imagens/sacola.svg";
-import "./style.css";
+import styled from "styled-components";
 
 const icones = [perfil, sacola];
+const Icone = styled.li`
+  margin-right: 40px;
+  width: 25px;
+  cursor: pointer;
+`;
+
+const Icones = styled.ul`
+  display: flex;
+  align-items: center;
+`;
 
 function IconesHeader() {
   return (
-    <ul className="icones">
-    {icones.map((icone) => (
-      <li className="icone">
-        <img src={icone} 
-        alt="icone"/>
-      </li>
-    ))}
-  </ul>
+    <Icones>
+      {icones.map((icone) => (
+        <Icone>
+          <img src={icone} alt="icone" />
+        </Icone>
+      ))}
+    </Icones>
   );
 }
 
--- src/components/logo/index.js
+++ src/components/logo/index.js
@@ -1,12 +1,21 @@
 import logo from '../../imagens/logo.svg';
-import './style.css'
+import styled from "styled-components";
+
+const LogoContainer = styled.div`
+  display: flex;
+  font-size: 30px;
+`;
+
+const LogoImage = styled.img`
+  margin-right: 10px;
+`;
 
 function Logo(){
     return(
-        <div className="logo">
-          <img src={logo} alt="logo" className='logo-img'/>
+        <LogoContainer>
+          <LogoImage src={logo} alt="logo"/>
           <p> <strong>Zara</strong>Books</p>
-        </div>
+        </LogoContainer>
     )
 }
 
