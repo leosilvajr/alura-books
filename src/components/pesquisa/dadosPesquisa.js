f213335fbb9c73387b55f9e98f4f02f40e20f4c7
Leonardo Silva <106756853+leosilvajr@users.noreply.github.com>
2025-08-16 22:04:45 -0300

    Finalizado UseState

--- src/components/pesquisa/dadosPesquisa.js
+++ src/components/pesquisa/dadosPesquisa.js
@@ -1,9 +1,33 @@
-import livro from '../../imagens/livro.png'
-
 export const livros = [
-    {nome: 'Liderança em Design', id: 1, src:livro},
-    {nome: 'Liderança em UX', id: 1, src:livro},
-    {nome: 'JavaScript Assertivo', id: 2, src:livro},
-    {nome: 'Cangaceiro em JavaScript', id: 3, src:livro},
-    {nome: 'Aventureiros do Java', id: 4, src:livro},
-]
+    {
+      nome: 'JavaScript Eloquente – 3ª Edição',
+      id: 1,
+      src: 'https://m.media-amazon.com/images/I/91asIC1fRwL.jpg'
+    },
+    {
+      nome: 'Você Ainda Não Sabe JS – Introdução',
+      id: 2,
+      src: 'https://m.media-amazon.com/images/I/81kqrwS1nNL.jpg'
+    },
+    {
+      nome: 'Código Limpo',
+      id: 4,
+      src: 'https://m.media-amazon.com/images/I/41xShlnTZTL._SX374_BO1,204,203,200_.jpg'
+    },
+    {
+      nome: 'O Programador Pragmático – Edição de 20º Aniversário',
+      id: 7,
+      src: 'https://m.media-amazon.com/images/I/518FqJvR9aL._SX377_BO1,204,203,200_.jpg'
+    },
+    {
+      nome: 'Codigo Limpo – Revisado',
+      id: 8,
+      src: 'https://m.media-amazon.com/images/I/41jEbK-jG+L._SX379_BO1,204,203,200_.jpg'
+    },
+    {
+      nome: 'A Arte de Projetar Coisas do Dia a Dia – Edição Revisada',
+      id: 9,
+      src: 'https://m.media-amazon.com/images/I/41DNuJfahyL._SX331_BO1,204,203,200_.jpg'
+    }
+  ]
+  
